7d45a2653e39a81b6787f164e6b3df27f4a2b33d
RiadMefti <riadmefti@hotmail.com>
2024-07-02 07:39:05 -0400

    adding jwt create and validate

---
+++ backend/internal/auth-service/jwt.go
@@ -0,0 +1,56 @@
+package auth
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var jwtKey = []byte(os.Getenv("JWTSECRET"))
+
+// CustomClaims defines custom JWT claims containing user ID and email.
+type CustomClaims struct {
+	ID    string `json:"id"`
+	Email string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+// GenerateToken generates a new JWT token for a user.
+func GenerateToken(userID, email string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := CustomClaims{
+		ID:    userID,
+		Email: email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+			Issuer:    "the-get-organized-app",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+// ValidateToken validates the JWT token and returns the custom claims.
+func ValidateToken(tokenString string) (*CustomClaims, error) {
+	claims := &CustomClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	return claims, nil
+}
