65e157966621fef370e7abb6c2a844cf900bf13d
RiadMefti <riadmefti@hotmail.com>
2024-07-13 17:42:48 -0400

    migration for  goals and objectives

---
+++ backend/internal/migrations/20240711211807_create_objectives_goals.sql
@@ -0,0 +1,40 @@
+-- +goose Up
+-- +goose StatementBegin
+
+
+CREATE TABLE objectives (
+    id UUID PRIMARY KEY,
+    start_date DATE NOT NULL,
+    type VARCHAR(255) NOT NULL CHECK (type IN ('year', 'month', 'week')),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE goals (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    done BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE objectives_goals (
+    objective_id UUID NOT NULL,
+    goal_id UUID NOT NULL,
+    PRIMARY KEY (objective_id, goal_id),
+    FOREIGN KEY (objective_id) REFERENCES objectives (id) ON DELETE CASCADE,
+    FOREIGN KEY (goal_id) REFERENCES goals (id) ON DELETE CASCADE
+);
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+DROP TABLE objectives_goals;
+DROP TABLE goals;
+DROP TABLE objectives;
+
+-- +goose StatementEnd
